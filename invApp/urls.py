fa83593a52df5e417f0bbbf2633e3467d11da5e4
AmanNarendraAli <amannarendraali@gmail.com>
2026-02-12 13:52:28 +0530

    added urls and views

---
+++ invApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home_view,name='home'),
+    path('list/',views.read_view,name='productlist'),
+    path('create/',views.create_view,name='createproduct'),
+    path('update/<int:product_id>/',views.update_view,name='updateproduct'),
+    path('delete/<int:product_id>/',views.delete_view,name='deleteproduct')
+]
--- invApp/views.py
+++ invApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from .models import Product
 
+# CRUD routes
 # Create your views here.
+
+#Home View
+
+def home_view(request):
+    return render(request, 'home.html')
+
+#Create View
+
+def create_view(request):
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save
+            return redirect('productlist')
+    return render(request,'productform.html',{'form':form}) #if request is not a POST/submission, we just go back to the empty form template view
+
+#Read View
+
+def read_view(request):
+    products = Product.objects.all()
+    return render(request,'productlist.html',{'products':products})
+
+#Update View
+
+def update_view(request,product_id):
+    product = Product.objects.get(product_id=product_id)
+    form = ProductForm(instance=product)
+    if request.method=='POST':
+        form = ProductForm(request.POST,instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('productlist')
+    return render(request,'productform.html',{'form':form})
+#Delete View
